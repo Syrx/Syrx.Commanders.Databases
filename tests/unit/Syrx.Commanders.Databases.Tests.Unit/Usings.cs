af952767bc6569c4b0c98f97080d81aa3c670c19
David Sexton <sextondjc@users.noreply.github.com>
2024-11-21 15:55:17 +0000

    Simplified build, more tests
    Added Directory.Build.props to simplify build and package maintenance.
    Added additional validation for Execute with model overload.
    Added a new unit tests assembly for the DatabaseCommander. Have been relying on integration tests before this.

---
+++ tests/unit/Syrx.Commanders.Databases.Tests.Unit/Usings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using Syrx.Commanders.Databases.Connectors.Extensions;
+global using Syrx.Commanders.Databases.Tests.Extensions;
+global using Syrx.Extensions;
+global using System.Data.Common;
+global using static Xunit.Assert;
